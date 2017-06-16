cfb4c912d1e45b6f9acaa8bb92f6fbd848a4ddaa
cdufour <opusidea@gmail.com>
2017-06-16 17:39:27 +0200

    symfony suite

--- src/AppBundle/Controller/PlayerController.php
+++ src/AppBundle/Controller/PlayerController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Player;
 
 class PlayerController extends Controller
 {
@@ -23,12 +25,46 @@ class PlayerController extends Controller
 
         $joueurs = [$joueur1, $joueur2, $joueur3];
 
+        // chargement des joueurs depuis la base de données
+        // Récupération du repository
+        $repository = $this
+                        ->getDoctrine()
+                        ->getManager()
+                        ->getRepository('AppBundle:Player');
+
+        $players = $repository->findAll();
+
         return $this->render('player/index.html.twig', array(
             'title'         => $title,
             'message'       => 'Symfony semble formidable',
             'joueur1'       => $joueur1,
-            'joueurs'       => $joueurs
+            'joueurs'       => $joueurs,
+            'players'       => $players
         ));
+    }
+
+    /**
+     * @Route("/player/add", name="addplayer")
+     */
+    public function addAction(Request $request)
+    {
+        $player = new Player();
+        $player->setNom("Totti");
+        $player->setPrenom("Francesco");
+        $player->setAge(40);
+
+        // récupération de l'Entity Manager
+        // objet permettant in fine d'intéragir avec la base
+        $em = $this->getDoctrine()->getManager();
+
+        // étape 1 : on "persiste" les données => enregistrement
+        $em->persist($player);
+
+        // étape 2 : nettoyage
+        $em->flush();
+
+        // on DOIT retourner une réponse HTTP au client
+        return new Response('joueur ajouté avec succès');
     }
 
 }
---
+++ src/AppBundle/Entity/Player.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Player
+ *
+ * @ORM\Table(name="player")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PlayerRepository")
+ */
+class Player
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="age", type="integer")
+     */
+    private $age;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Player
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return Player
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set age
+     *
+     * @param integer $age
+     *
+     * @return Player
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return int
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+}
+
